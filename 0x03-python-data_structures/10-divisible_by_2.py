73582fc6e7112dbe202ff8edf3ade6cff0f9fe49
Patrick-052 <muiruripatrick052@gmail.com>
2023-03-13 19:55:19 +0300

    Find the max

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    new_list = ''
     for y in range(len(my_list)):
-        if my_list[y] % 2 == 0:
+        if (new_list[y]) % 2 == 0:
             return True
     return False
