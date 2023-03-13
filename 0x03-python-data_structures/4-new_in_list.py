231421318190fa2ad93f2e9f1bf0b5b001d9efde
Patrick-052 <muiruripatrick052@gmail.com>
2023-03-13 17:00:49 +0300

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()
-    new_list = my_list.copy
-    new_list[idx] = element
-    return new_list
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+    copy = [d for d in my_list]
+    copy[idx] = element
+    return (copy)
