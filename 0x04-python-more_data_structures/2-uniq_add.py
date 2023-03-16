215be911a1f04b9cda0cb68cf25f9767847ade0a
Patrick-052 <muiruripatrick052@gmail.com>
2023-03-16 21:23:50 +0300

    Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_list = []
+    sum = 0
+    for i in my_list:
+        if i not in uniq_list:
+            uniq_list.append(i)
+            sum += i
+    return sum
