6a9dbc3a2793c0d939b2c6cc7f7f0d3b800aaeb0
Patrick-052 <muiruripatrick052@gmail.com>
2023-03-16 23:28:52 +0300

    Multiply by 2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_key = None
-    max_value = float(-'inf')
-    for key, value in a_dictionary.items():
-        if value > max_value:
-            max_key = key
-            max_value = value
-    return max_key
+    if not a_dictionary:
+        return (None)
+    return (max(a_dictionary, key=a_dictionary.get))
