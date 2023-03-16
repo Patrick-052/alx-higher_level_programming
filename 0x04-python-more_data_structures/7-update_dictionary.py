280238153e4899e3f6b8b85a202d32b1f323cd3f
Patrick-052 <muiruripatrick052@gmail.com>
2023-03-16 22:49:04 +0300

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for k, v in a_dictionary.items():
-        if k == key:
-            a_dictionary[k] = new_value
-            return a_dictionary
     a_dictionary[key] = value
     return a_dictionary
