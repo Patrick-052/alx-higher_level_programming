9181b5cd892e2bdc13158c4fad9337f140adbbdd
Patrick-052 <muiruripatrick052@gmail.com>
2023-03-27 20:08:42 +0300

    integers list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value % 2 == 0:
-            print("{:d}".format(value, end=""))
-    except ValueNotInteger:
+         if isinstance(value, int) and value % 2 == 0:
+             print("{:d}".format(value))
+    except TypeError:
         return False
     else:
         return True
-    print()
