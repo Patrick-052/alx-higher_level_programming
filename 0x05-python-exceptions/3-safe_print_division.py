c10a6edbcccab6e8c762e94c5a2768f351226015
Patrick-052 <muiruripatrick052@gmail.com>
2023-03-27 21:23:50 +0300

    division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+        print("Can't divide by zero")
+    finally:
+        print("Inside result: {}".format(result))
+    return result
