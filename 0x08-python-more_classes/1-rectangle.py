50f9c82a18f78a04246e2abf9cd91762c8eed703
Patrick-052 <muiruripatrick052@gmail.com>
2023-04-03 16:36:04 +0300

    Real definition

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,21 +3,17 @@
 
 
 class Rectangle:
-
     """Initialization of the attributes"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-
     """Retrieving the width"""
     @property
     def width(self):
         return self.__width
-
     """Setting the width"""
     @width.setter
     def width(self, value):
-
         """Raise error if present"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer");
@@ -25,15 +21,14 @@ class Rectangle:
             raise ValueError("width must be >= 0");
         self.__width = value
 
-    """Retrieving the height"""
+        """Retrieving the height"""
     @property
     def height(self):
         return self.__height
-
+    
     """Setting the height"""
     @height.setter
     def height(self, value):
-
         """Raise error if present"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer");
