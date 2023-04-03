15c95739bb4053946eb3b34cd72e559f633de6b7
Patrick-052 <muiruripatrick052@gmail.com>
2023-04-03 16:26:06 +0300

    Real definition

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Rectangle class to represent the width and height"""
+
+
+class Rectangle:
+
+    """Initialization of the attributes"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    """Retrieving the width"""
+    @property
+    def width(self):
+        return self.__width
+    
+    """Setting the width"""
+    @width.setter
+    def width(self, value):
+
+        """Raise error if present"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer");
+        if value < 0:
+            raise ValueError("width must be >= 0");
+        self.__width = value
+    
+    """Retrieving the height"""
+    @property
+    def height(self):
+        return self.__height
+    
+    """Setting the height"""
+    @height.setter
+    def height(self, value):
+
+        """Raise error if present"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer");
+        if value < 0:
+            raise ValueError("height must be >= 0");
+        self.__height = value
