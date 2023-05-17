28b2089d529f0bc1d35a49963f500835a8c0a4a0
Patrick-052 <muiruripatrick052@gmail.com>
2023-05-18 00:09:18 +0300

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id Asc;
+ORDER BY tv_shows.title Asc, tv_show_genres.genre_id Asc;
