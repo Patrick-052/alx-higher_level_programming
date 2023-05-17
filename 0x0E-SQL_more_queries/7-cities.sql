ddcb5c6d9a72ab1a690b922c0ede004b9362bf84
Patrick-052 <muiruripatrick052@gmail.com>
2023-05-17 22:41:23 +0300

    States table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL
-    CONSTRAINT FOREIGN KEY(state_id) REFERENCES state(id),
+    FOREIGN KEY (state_id) REFERENCES state (id),
     name VARCHAR(256) NOT NULL
 );
