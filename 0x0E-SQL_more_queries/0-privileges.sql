98da84702c67d4a80297e6318b0dab85570953b5
Patrick-052 <muiruripatrick052@gmail.com>
2023-05-17 19:09:37 +0300

    my first commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- script that lists all privileges of the MySQL users user_0d_1
 -- and user_0d_2 on your server (in localhost)
-SHOW GRANTS FOR 'user_0d_1'@'malcom' AND 'user_0d_2'@'malcom';
+SHOW GRANTS FOR 'user_0d_1'@'12514' AND 'user_0d_2'@'12514';
