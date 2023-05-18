b39803d93308fdd536c09ddd250a7c911edd4b7b
Patrick-052 <muiruripatrick052@gmail.com>
2023-05-18 10:07:37 +0300

    Temperatures 1

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+--script that displays the top 3 of cities temperature
+--during July and August ordered by temperature (descending)
+SELECT city, temperature
+FROM temperatures
+WHERE month IN ('July', 'August')
+ORDER BY temperature Desc
+LIMIT 3;
