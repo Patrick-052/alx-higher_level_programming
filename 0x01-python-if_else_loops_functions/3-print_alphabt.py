af40c06beb0ce3ab45bf98e59b11fcecbbde930a
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-03 21:35:21 +0300

    alphabet soup

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == 101 or| i == 113:
-        continue
-    else:
-        print("{:c}".format(i), end="")
+
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{:s}".format(chr(i)), end='')
