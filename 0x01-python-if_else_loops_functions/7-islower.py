2159f59953c9def6750eb9047b3f44f2167cb373
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-04 00:34:38 +0300

    islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def islower(c):
+    for i in range(ord('a'), ord('z') + 1):
+        if chr(i) == 'c':
+            return True
+    return False
+
+
+c = 'c'
+result = islower(c)
+print(result)
