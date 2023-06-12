bc1c00cd9f21c307403fbd47319af242dcf14d41
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-12 21:12:46 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ const argument = process.argv;
 
 if (argument.length === 2) {
   console.log('No argument');
-} else if (argument.length > 2) {
+} else if (argument.length === 3) {
+  console.log('Argument found');
+} else {
   console.log('Argument found');
 }
