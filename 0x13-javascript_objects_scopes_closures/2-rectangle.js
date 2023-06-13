183c144f2ddd2544881f36dddd69691c4fa5f00f
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-13 20:38:25 +0300

    empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (!Number.isInteger(w) || w <= 0 || !Number.isInteger(h) || h <= 0) {
       return;
     }
     this.width = w;
