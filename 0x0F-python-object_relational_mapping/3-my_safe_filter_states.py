0f054473fd617c4598c3f38d2d7d5c1e4a623e61
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-18 14:15:40 +0300

    avoiding sql injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Retriving a specific item specified in the command line and
+   preventing the possibility of injections
+   usage: ./2-my_filter_states.py <mysql username>
+                                  <mysql password>
+                                  <database name>
+                                  <state name searched>"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE binary '{}'
+                 ORDER BY id ASC""".format(argv[4].strip("'")))
+    query_rows = cur.fetchall()
+    [print(row) for row in query_rows]
