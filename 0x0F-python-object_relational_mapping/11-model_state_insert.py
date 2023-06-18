dc077c1065af2b35281f4dc75c2b11a33d4718fe
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-19 01:34:51 +0300

    session adding

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Adds and retrieves the id of the added object"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+             "mysql+mysqldb://{}:{}@localhost/{}"
+             .format(argv[1], argv[2], argv[3]),
+             pool_pre_ping=True)
+
+    session = sessionmaker(bind=engine)
+    session = session()
+    Louisiana = State("Louisiana")
+    session.add(Louisiana)
+    states = session.query(State).filter(State.name == Louisiana)\
+                    .order_by(State.id).first()
+    result = [states.id if states else "Not found"]
+    print(result)
