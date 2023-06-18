83485d5a33f17f24f81c607af9e49182abd94b64
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-18 09:29:52 +0300

    listing specific states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,9 +7,6 @@ from sys import argv
 if __name__ == "__main__":
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    [print(row) for row in query_rows if row[1][0] == "N"]
