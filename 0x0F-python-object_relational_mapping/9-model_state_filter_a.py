b3a3cb52fcc1a6aa4c3dd16cb6c09c9bdf2f685e
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-19 00:24:57 +0300

    State names containing (a)

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Retrieving state objects from the database that contain
+   (a) iin their name"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine(
+             "mysql+mysqldb://{}:{}@localhost/{}"
+             .format(argv[1], argv[2], argv[3]),
+             pool_pre_ping=True)
+
+    session = sessionmaker(bind=engine)
+    session = session()
+    states = session.query(State).filter(text("a in State.name")).all()
+    [print("{}: {}".format(state.id, state.name)) for state in states]
