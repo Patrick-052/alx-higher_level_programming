d4e056fa0c32b6ca01ea083ae9078fa61b2d0d1a
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-19 13:12:04 +0300

    Wow Cities in state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost/
+    {}""".format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                 pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    Rt = session.query(State).join(City)\
+                .filter(City.state_id == State.id).order_by(City.id).all()
+    [print("{}: ({}) {}".format(i[1], i[2], i[4])) for i in Rt]
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Mapping city class to cities table in the database"""
+
+from model_state import Base, State
+from sqlalchemy import Column, ForeignKey, String, Integer
+
+
+class City(Base):
+    """Inherits from Base and defines the following class attributes:
+        __tablename__ => defines name of the table
+        id => represents a column of an auto-generated, unique integer,
+        can't be null and is a primary key
+        name => represents a column of a string of 128 characters and
+        can't be null
+        state_id => represents a column of an integer, can't be null
+        and is a foreign key to states.id
+    """
+
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
