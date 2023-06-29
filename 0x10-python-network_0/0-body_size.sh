909e4309640851e0bc324f9c62df458ce52a6c4a
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-29 21:34:41 +0300

    Finding out the size of a response output

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# Sending a request to a URL and
-#+displaying the size of the body of the response
+# displaying the size of the response
 
-curl -L -s http://www.youtube.com/ | wc -c
+curl -L -s | wc -c
