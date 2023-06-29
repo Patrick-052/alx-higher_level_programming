f9bad47db133616ef2d10e14dabd02533c6378a1
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-30 00:54:30 +0300

    Finding out the peak of a sequence of integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Function to obtain the peak/greatest value
+   in a sequence of integers"""
+
 
 def find_peak(list_of_integers):
-    """Function to obtain the peak/greatest value
-       in a sequence of integers"""
-    return max(list_of_integers)
+    gret = sorted(list_of_integers)
+    return (gret[-1])
