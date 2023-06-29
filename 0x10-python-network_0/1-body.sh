0d3056505a5ca51e41a2219491f4cebb53f9bc4c
Patrick-052 <muiruripatrick052@gmail.com>
2023-06-29 22:17:11 +0300

    Retrieving only the body of a response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #displaying the body of the response obtained by (GET)
-curl -sL -w "%{http_code}\n" -o /dev/null | grep -q "200" && curl -sL
+curl -sL -w "%{http_code}\n" http://patrick-52.tech -o /dev/null | grep -q "200" && curl -sL http://patrick-52.tech
