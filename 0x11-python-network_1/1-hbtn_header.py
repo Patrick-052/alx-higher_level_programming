8eec3c609df1c2093ce553ef14488d01e37322e0
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-01 12:27:15 +0300

    Request id for every request made to a url

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Displaying a different value of X-Request-Id
+   on every call of the request"""
+import urllib.request as ur
+from sys import argv
+
+
+if __name__ == "__main__":
+    with ur.urlopen(argv[1]) as res:
+        header = res.headers['X-Request-Id']
+        print(header)
