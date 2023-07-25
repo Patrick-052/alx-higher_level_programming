770a9087d09489bd0eb6d431063be945f424f29d
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-25 19:16:04 +0300

    Javascript can write

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 /* Writing to a file */
 
-const process = require('process');
-const arg = process.argv;
 const fs = require('fs');
+const arg = process.argv;
 
 fs.writeFile(arg[2], arg[3], 'utf-8', (error) => {
-  if (error) throw error;
-  console.log('saved!');
+  if (error) {
+  console.log(error);
+  }
 });
