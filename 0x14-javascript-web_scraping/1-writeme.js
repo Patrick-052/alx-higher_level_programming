61b6a6f2d0eabcd23db51f2b832752df200d01b4
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-25 19:03:36 +0300

    Javascript can write

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,9 +6,6 @@ const arg = process.argv;
 const fs = require('fs');
 
 fs.writeFile(arg[2], arg[3], 'utf-8', (error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('saved!');
-  }
+  if (error) throw error;
+  console.log('saved!');
 });
