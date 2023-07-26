8d335888c9d088c6f7a2c7128c456e1910e4571c
Patrick-052 <muiruripatrick052@gmail.com>
2023-07-26 12:19:35 +0300

    Updates of fixed errors

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,21 +5,19 @@ computeting tasks completed by userid */
 const request = require('request');
 const url = process.argv[2];
 
-request.get(url, { json: true }, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
-    return;
   }
 
-  const tasksCompleted = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!tasksCompleted[todo.userId]) {
-        tasksCompleted[todo.userId] = 1;
-      } else {
-        tasksCompleted[todo.userId] += 1;
-      }
+  const data = JSON.parse(body);
+  const userCounts = {};
+
+  data.forEach((task) => {
+    if (task.completed) {
+      const userId = task.userId;
+      userCounts[userId] = (userCounts[userId] || 0) + 1;
     }
   });
-  console.log(tasksCompleted);
+  console.log(userCounts);
 });
